1f17a4f7e606c95d9e6f968156dc6776d2f46185
LunarKhord <krazygenus@gmail.com>
2026-07-22 14:47:14 +0100

    (Chore): Add more fields to the Pydantic class

--- src/models/wait_list.py
+++ src/models/wait_list.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel, EmailStr, Field
-
+from typing import Optional
 
 
 class WaitList(BaseModel):
-    email:EmailStr = Field(..., description="Email of interested user", examples=["somethingraw@gmail.com"])
+    business_name: str = Field(..., description="Name of the business", examples=["My Business"])
+    work_email:EmailStr = Field(..., description="Email of interested user", examples=["somethingraw@gmail.com"])
+    company_size: str = Field(..., description="Size of the company", examples=["1-10", "11-50", "51-200", "201-500", "501-1000", "1001+"])
+    current_invoice_tool: Optional[str] = Field(None, description="Current invoice tool used by the business", examples=["QuickBooks", "FreshBooks", "Xero"])
+    
